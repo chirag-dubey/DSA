fe77951d5e69e03d8276007f97b1827b8a3c50e9
chirag-dubey <chiragdubey9818@gmail.com>
2026-07-17 11:20:28 +0530

    Time: 11 ms (5.27%) | Memory: 16.4 MB (80.75%) - LeetSync

---
+++ 179-largest-number/largest-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+static bool Dsec(int s1,int s2){
+    string f=to_string(s1)+to_string(s2);
+    string s=to_string(s2)+to_string(s1);
+    if(f>s){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end(),Dsec);
+        string sum="";
+        for( int i=0;i<nums.size();++i){
+            sum+=to_string(nums[i]);
+        }
+        if(!sum.empty() && sum[0]=='0'){
+            return "0";
+        }
+        return sum;
+    }
+};
